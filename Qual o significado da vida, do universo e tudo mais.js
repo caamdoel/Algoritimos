4985437bbb97311a1bd4afdd08e82b3c94e1ef08
caamdoel <89041061+caamdoel@users.noreply.github.com>
2021-08-22 22:06:43 -0300

    Add files via upload

---
+++ Qual o significado da vida, do universo e tudo mais.js
@@ -0,0 +1,10 @@
+/*Qual o sifnificado da vida, do universo e tudo mais/*
+/*requisito:
+/Criar o programa "Qual o significado da vida, do universo e tudo mais?". 
+Ao clicar, deve aparecer no console o número do universo.
+O programa terá uma variável inteira, com identificador "universo", onde voce irá atribuir o número em questão.
+*/
+
+var  Universo  =  42;
+console.log("Qual o significado da vida, do universo e tudo mais?");
+console.log(Universo);
