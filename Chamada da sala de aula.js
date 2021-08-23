d674207d5024119d9d42a98a9ef2ef459359f17d
caamdoel <89041061+caamdoel@users.noreply.github.com>
2021-08-22 22:10:07 -0300

    Add files via upload

---
+++ Chamada da sala de aula.js
@@ -0,0 +1,50 @@
+/*Chamada da sala de aula*/
+/*requisito:
+/O programa "Chamada da sala de aula" irá escrever o (primeiro) de todos os seus colegas,
+em ordem alfabética decrescente.
+Para ter acesso à lista dos nomes, voçe pode olhas a listagem da turma no Classroom.
+Dica: Para fazer a ordenação da lista na tela, utilize as teclas de atalho para "mover linhas" no VSCode.
+*/
+
+var  alunos  =  
+[
+    'Viviane' ,
+    'Victor' ,
+    'Vanderson' ,
+    'Samuel' ,
+    'Rodrigo' ,
+    'Rodrigo' ,
+    'Raquel' ,
+    'Rafael' ,
+    'Pedro' ,
+    'Pedro' ,
+    'Núria' ,
+    'Nathan' ,
+    'Matheus' ,
+    'Marcos' ,
+    'Marcos' ,
+    'Marcella' ,
+    'Luciana' ,
+    'Luciana' ,
+    'Luana' ,
+    'Leandro' ,
+    'Karina' ,
+    'Higor' ,
+    'Gustavo' ,
+    'Guilherme' ,
+    'Gilnei' ,
+    'Eduardo' ,
+    'Diego' ,
+    'Daniel' ,
+    'Carolina' ,
+    'Carlos' ,
+    'Bruno' ,
+    'Beatriz' ,
+    'Angelita' ,
+    'Andre' ,
+    'Andre' ,
+    'Adrian'
+  ]
+  
+  console . log ( alunos ) ;
+  
