d99a7c2912d8a71fd8cb020db28d23f66cedce44
caamdoel <89041061+caamdoel@users.noreply.github.com>
2021-08-22 22:09:01 -0300

    Add files via upload

---
+++ Carta para leitura final do curso.js
@@ -0,0 +1,11 @@
+/*Algoritimos
+Escreva uma carta para você mesmo ler ao final do curso,
+e imprima sem console. Na carta você deve incluir como suas
+expectativas para o curso de programação e sua carreira
+nesta nova área. Seja criativo, lembre-se de incluir
+desafios e incentivos, para que o seu eu do futuro tenha
+orgulho de suas conquistas! */
+
+
+var  carta  =  "Seus medos já não existem. \ nSuas informações obtidas para trás. \ nVocê conseguiu que não conseguiria. \ nMas conseguiu! \ nÉ só o começo da caminhada, se esforce e tenha disposição. \ nAssim você alcançará tudo que almeja. " ;
+ console.log (carta);
---
+++ Um amigo tres palavras.js
@@ -0,0 +1,7 @@
+/*Um amigo meu disse que conseguir escrever três palavras diferentes
+sem tirar uma caneta do papel.
+No caso do nosso programa, o comando de escrita só pode ser chamado uma vez,
+porém as três mensagens devem ser separadas no console.*/
+	
+console.log("Seguem as três palavras: \ nPapel, \ nCaneta, \ nBorracha");
+	
