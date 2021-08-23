6e83d7e6aa0d6c86b35f38a7bc21f87018e5c586
caamdoel <89041061+caamdoel@users.noreply.github.com>
2021-08-22 22:05:26 -0300

    Add files via upload

---
+++ Sobre a Empresa Alterda.js
@@ -0,0 +1,9 @@
+/*Sobre a Empresa Alterda"/
+/*requisito:
+/Escreva o programa "Sobre a Empresa Alterdata". Sua saída sera a mensagem:
+"A Alterdata tem x anos.",onde o x é a idade da empresa.
+A idade será atribuida em uma variável inteira.
+*/
+
+var  idade  =  32 ;
+console. log( "A Alterdata tem",idade,"anos.");
