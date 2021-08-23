ea55a49a6212897d2c2e9833991768cc43dbd6e3
caamdoel <89041061+caamdoel@users.noreply.github.com>
2021-08-23 10:25:51 -0300

    Update Carta para leitura final do curso.js

--- Carta para leitura final do curso.js
+++ Carta para leitura final do curso.js
@@ -1,6 +1,6 @@
 /*Algoritimos
 Escreva uma carta para você mesmo ler ao final do curso,
-e imprima sem console. Na carta você deve incluir como suas
+e imprima no console. Na carta você deve incluir como suas
 expectativas para o curso de programação e sua carreira
 nesta nova área. Seja criativo, lembre-se de incluir
 desafios e incentivos, para que o seu eu do futuro tenha
